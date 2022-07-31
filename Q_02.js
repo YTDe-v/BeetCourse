ad13c5536f176a6d636b6a541ec77c7e761dad01
YT <YT@DEV.com>
2022-07-31 21:47:18 +0200

    first commit

---
+++ Q_02.js
@@ -0,0 +1,11 @@
+const modal=document.querySelector(".modal");
+
+function close_modal() {
+    modal.style.display = "none";
+    
+}
+
+function open_modal() {
+    modal.style.display = "block";
+}
+
